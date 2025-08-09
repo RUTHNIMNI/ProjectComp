94ba303ffc4b3a1ad48d44fceb6b1dc90928e279
Ruth <nimni105@gmail.com>
2025-08-09 19:41:11 +0300

    - Add done to a completed ToDo.

--- src/ProjectComp/TodoItem.jsx
+++ src/ProjectComp/TodoItem.jsx
@@ -63,17 +63,21 @@ const TodoItem = ({ task, removeTask, toggleComplete }) => { // מקבל props: 
     style={{
       marginRight: '10px',
       // אם נשארו 2 ימים או פחות, או שהדד-ליין עבר, הצבע יהיה אדום. אחרת, צבע חום-צהבהב.
-      color: (daysLeft <= 2) ? '#ff1a1a' : '#b67c31', // אדום אם 2 ימים או פחות, אחרת חום
+      color: (daysLeft <= 2 && !task.completed) ? '#ff1a1a' : '#b67c31', // אדום אם 2 ימים או פחות, אחרת חום
       fontWeight: 'bold'
     }}
   >
-          {/* אם נשארו ימים, מציג את מספר הימים. אם עבר הדד-ליין, מציג הודעה מתאימה */}
-           {daysLeft >= 0
-          ? `Days left: ${daysLeft} ${daysLeft === 1 ? 'day' : 'days'}` // If days left
-          : 'Deadline passed' // אם הדד ליין עבר
+    {/* אם המשימה הושלמה, מציג "Done". אחרת, מציג את מספר הימים או הודעת דד-ליין */}
+    {task.completed
+      ? 'Done'
+      : (
+          daysLeft >= 0
+            ? `Days left: ${daysLeft} ${daysLeft === 1 ? 'day' : 'days'}` // If days left
+            : 'Deadline passed' // אם הדד ליין עבר
+        )
     }
-        </span>
-      )}
+  </span>
+)}
 
 
 <button
